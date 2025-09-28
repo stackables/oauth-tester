2f5909d9680768b61068d77545b4f96812684b98
Aarne Laur <aarne.laur@gmail.com>
2025-09-28 15:22:17 +0200

    Visual styles

--- src/app/oauth2-auth/page.tsx
+++ src/app/oauth2-auth/page.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
@@ -6,10 +6,16 @@ import { Separator } from "@/components/ui/separator";
 import { Faker, en } from "@faker-js/faker";
 import { useSearchParams } from "next/navigation";
 import { useMemo, useState } from "react";
-import { InfoIcon } from 'lucide-react'
-import { Dialog, DialogContent, DialogHeader, DialogTitle } from "@/components/ui/dialog";
-import { useLocalStorage } from 'usehooks-ts'
-import dynamic from 'next/dynamic'
+import { InfoIcon } from "lucide-react";
+import {
+  Dialog,
+  DialogContent,
+  DialogHeader,
+  DialogTitle,
+} from "@/components/ui/dialog";
+import { useLocalStorage } from "usehooks-ts";
+import dynamic from "next/dynamic";
+import { cn } from "@/lib/utils";
 
 const faker = new Faker({ locale: [en] });
 
@@ -37,11 +43,6 @@ function getTestUsers(start: number, stable: number, unstable: number) {
 
   const users = [];
 
-  if (start !== 0) {
-    stable += unstable - 1;
-    unstable = 1;
-  }
-
   for (let i = 0; i < stable; i++) {
     faker.seed(start + 849 + i);
     users.push(generate(true));
@@ -63,7 +64,6 @@ function Oauth2Auth() {
   const state = searchParams.get("state");
   const [pin, setPin] = useLocalStorage("pin", 0);
   const [dialog, setDialog] = useState<Record<string, unknown> | null>(null);
-  
 
   const params = [];
   if (searchParams) {
@@ -73,14 +73,15 @@ function Oauth2Auth() {
   }
 
   const users = useMemo(() => {
-    return getTestUsers(pin, 4, 3)
+    const total = 9;
+    const unstable = pin > 0 ? 2 : 9;
+
+    return getTestUsers(pin, total - unstable, unstable);
   }, [pin]);
 
   return (
-    <div className="grid grid-cols-1 gap-2 p-4">
-      <h1 className="text-2xl font-bold">
-        User Picker
-      </h1>
+    <div className="grid grid-cols-1 gap-2 p-4 max-w-4xl mx-auto">
+      <h1 className="text-2xl font-bold">User Picker</h1>
       <Input
         id="pin"
         placeholder={`PIN = ${pin}`}
@@ -91,11 +92,15 @@ function Oauth2Auth() {
       <Separator />
       <div className="flex flex-row gap-2">
         <Button asChild variant={"destructive"} className="flex-grow">
-          <a href={`${searchParams?.get("redirect_uri")}?error=access_denied&state=${state}`}>Decline</a>
+          <a
+            href={`${searchParams?.get("redirect_uri")}?error=access_denied&state=${state}`}
+          >
+            Decline
+          </a>
         </Button>
         <Button
-        className="flex-grow"
-        variant={"default"}
+          className="flex-grow"
+          variant={"default"}
           onClick={() => {
             setPin(0);
           }}
@@ -106,24 +111,30 @@ function Oauth2Auth() {
       <Separator />
       {users.map((user, i) => (
         <div key={user.sub} className="flex flex-row gap-2">
-            <Button
-              key={user.sub}
-              data-testid={`test-user-${i}`}
-              asChild
-              variant={"outline"}
-              className="flex-grow"
+          <Button
+            key={user.sub}
+            data-testid={`test-user-${i}`}
+            asChild
+            variant={"outline"}
+            className={cn("flex-grow", {
+              "border-amber-500/50": user.stable,
+            })}
+          >
+            <a
+              href={`${searchParams?.get("redirect_uri")}?code=${objectToBase64(user)}&state=${state}`}
             >
-              <a href={`${searchParams?.get("redirect_uri")}?code=${objectToBase64(user)}&state=${state}`}>{user.name}</a>
-            </Button>
-            <Button
-              onClick={() => setDialog(user)}
-              variant={"ghost"}
-            >
-              <InfoIcon  />
-            </Button>
+              {user.name}
+            </a>
+          </Button>
+          <Button onClick={() => setDialog(user)} variant={"ghost"}>
+            <InfoIcon />
+          </Button>
         </div>
       ))}
-      <Dialog open={dialog !== null} onOpenChange={(open) => setDialog(open === false ? null : dialog)}>
+      <Dialog
+        open={dialog !== null}
+        onOpenChange={(open) => setDialog(open === false ? null : dialog)}
+      >
         <DialogContent>
           <DialogHeader>
             <DialogTitle>User Details</DialogTitle>
@@ -138,5 +149,5 @@ function Oauth2Auth() {
 }
 
 export default dynamic(() => Promise.resolve(Oauth2Auth), {
-  ssr: false
-})
+  ssr: false,
+});
